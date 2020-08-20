29797bd7df33fd6c7782d313d6413202b87440cf
Natã Hermes <nhermesdev@gmail.com>
2020-08-19 22:05:43 -0300

    :rocket: Challenge concluded. Create transactions and list total income, outcome and balance, import files and save/delete from database.

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransectionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+    const transactionRepository = getCustomRepository(TransectionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
